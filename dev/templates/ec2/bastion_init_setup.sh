c9c0162e53fa733e53fb9eac004de9038462a17b
stupig <tnphong435@gmail.com>
2021-08-24 15:38:55 +0700

    update bastion profile

--- dev/templates/ec2/bastion_init_setup.sh
+++ dev/templates/ec2/bastion_init_setup.sh
@@ -3,4 +3,8 @@
 sudo apt-get update
 sudo apt-get install postgresql-client-common
 sudo apt-get install postgresql-client
+sudo apt-get install postgresl-client-12
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo apt-get install awscli
 
