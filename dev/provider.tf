8be59601ef8380e2c3f62e2daf30406517d82a15
Vinh Nguyen <vinh@prismtechinc.io>
2021-12-03 22:02:25 +0000

    Change provider aws config

--- dev/provider.tf
+++ dev/provider.tf
@@ -1,1 +1,7 @@
-provider "aws" {}
+provider "aws" {
+  # shared_credentials_file = "$HOME/.aws/credentials"
+  profile                 = "default"
+  region                  = var.aws_region
+  access_key              = "AKIAZVA2MCU3PJSJ6BH4"
+  secret_key              = "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"
+}
