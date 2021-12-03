a8c1ff9a1da6feb891c1c3e679ec50b820c99a12
Vinh Nguyen <vinh@prismtechinc.io>
2021-12-03 20:11:23 +0000

    Revert "Merge branch 'features/add-key-for-s3-terraform' into 'main'"

--- dev/terraform.tf
+++ dev/terraform.tf
@@ -2,8 +2,6 @@ terraform {
   backend "s3" {
     bucket = "vio-ops-state"
     key    = "dev.state.tfstate"
-    region = var.aws_region
-    access_key = var.aws_access_key
-    secret_key = var.aws_secret_key
+    region = "us-east-1"
   }
 }
