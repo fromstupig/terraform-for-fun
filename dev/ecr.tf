e9957d22ae3a7ddc5fd18e66238ad57b5a4ec76b
Vinh Nguyen <vinh@prismtechinc.io>
2021-12-03 09:14:50 +0000

    Add ecr repository: vio/ofa

--- dev/ecr.tf
+++ dev/ecr.tf
@@ -33,3 +33,12 @@ resource "aws_ecr_repository" "analytics" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_repository" "ofa" {
+  name = "vio/ofa"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
