3bece73111abf493cba5204154a4c4a153e6bfa0
Vinh Nguyen <vinh.ngxv@gmail.com>
2021-12-06 11:28:41 +0700

    Update django db migration ecs task

--- dev/ecs.tf
+++ dev/ecs.tf
@@ -98,30 +98,30 @@ resource "aws_ecs_service" "celery" {
     module.db]
 }
 
-#data "template_file" "migration" {
-#  template = file("./templates/ecs/migration.json.tpl")
-#
-#  vars = {
-#    app_name = "vio-migration"
-#    app_image = var.dbmigration_image
-#    fargate_cpu = var.dbmigration_fargate_cpu
-#    fargate_memory = var.dbmigration_fargate_memory
-#    aws_region = var.aws_region
-#  }
-#}
+data "template_file" "migration" {
+  template = file("./templates/ecs/migration.json.tpl")
 
-#resource "aws_ecs_task_definition" "migration" {
-#  family = "vio-dbmigration-task"
-#  task_role_arn = aws_iam_role.ecs_services_access_role.arn
-#  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
-#  network_mode = "awsvpc"
-#  requires_compatibilities = [
-#    "FARGATE"]
-#  cpu = var.dbmigration_fargate_cpu
-#  memory = var.dbmigration_fargate_memory
-#  container_definitions = data.template_file.migration.rendered
-#}
-#
+  vars = {
+    app_name = "vio-migration"
+    app_image = var.dbmigration_image
+    fargate_cpu = var.dbmigration_fargate_cpu
+    fargate_memory = var.dbmigration_fargate_memory
+    aws_region = var.aws_region
+  }
+}
+
+resource "aws_ecs_task_definition" "migration" {
+  family = "vio-dbmigration-task"
+  task_role_arn = aws_iam_role.ecs_services_access_role.arn
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  network_mode = "awsvpc"
+  requires_compatibilities = [
+    "FARGATE"]
+  cpu = var.dbmigration_fargate_cpu
+  memory = var.dbmigration_fargate_memory
+  container_definitions = data.template_file.migration.rendered
+}
+
 #resource "aws_ecs_service" "migration" {
 #  name = "vio-dbmigration-service"
 #  cluster = aws_ecs_cluster.main.id
